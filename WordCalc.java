e5a16c3316737a7f4ed59122568af6c4813f77ae
buhcheva <darka.buh@gmail.com>
2014-01-08 18:02:10 +0100

    Update WordCalc.java

--- WordCalc.java
+++ WordCalc.java
@@ -1,10 +1,31 @@
+import java.util.HashMap;
+
+
 public class WordCalc {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashMap<String, Integer> map = new HashMap<String, Integer> ();
+		
+		
+		map.put("one", 1);
+		map.put("two", 2);
+		map.put("three", 3);
+		map.put("four", 4);
+		
+		System.out.println("Enter a number in a string form: ");
+		
+		for (String s: args) {
+            if (map.containsKey(s))
+            {
+            	System.out.println(map.get(s));
+            };
+        }
+		
+		
 
 	}
 
